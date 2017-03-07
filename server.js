f8dcb79973277b47be10e9f55dfaa601519d4632
ralfzimmermann <ralf.zimmermann@DEFR5143.local>
2017-03-07 18:24:57 +0100

    update reaction

--- server.js
+++ server.js
@@ -66,11 +66,22 @@ function getReactionGif(msg){
     xhr.send();
 
 }
+// slapp.event('file_shared', (msg) => {
+//
+// 	msg.say(":thumbsup:");
+// 	getReactionGif(msg);
+//
+// })
+
+// add a smile reaction by the bot for any message reacted to
 slapp.event('file_shared', (msg) => {
-
 	msg.say(":thumbsup:");
-	getReactionGif(msg);
-
+	let token = msg.meta.bot_token
+	let id = msg.body.event.item.ts
+	let channel = msg.body.event.item.channel
+	slapp.client.reactions.add({token, 'smile', id, channel}, (err) => {
+		if (err) console.log('Error adding reaction', err)
+	})
 })
 
 
